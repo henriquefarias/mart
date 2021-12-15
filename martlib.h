ea18528f8d6b2d07e3c9c1d21f6ff9713f070282
Henrique <18431250+henriquefarias@users.noreply.github.com>
2021-12-15 18:03:26 -0300

    Added binary search function

--- martlib.h
+++ martlib.h
@@ -91,14 +91,23 @@ PRODUCT prod_list[] =
 };
 
 
+/* ======================== VARIÁVEIS GLOBAIS ======================== */
+
+USER_LIST *users; /* Lista encadeada com registros de usuário */
+
+
 /* ============= PROTÓTIPOS DE FUNÇÕES ======================= */
 
 
 void build_user_dat(void);
 
+void load_users_dat(char *filename, USER_LIST *list);
+
+void write_users_dat(char *filename, USER_LIST *list);
+
 USER_LIST *build_user_list(int list_size);
 
-void busca_prontuario(char *prontuario, char tipo_busca);
+// void busca_prontuario(char *prontuario, char tipo_busca);
 
 void exit_program(void);
 
@@ -108,11 +117,7 @@ long int file_size(char *filename);
 
 int get_size(USER_LIST *list);
 
-int get_size(USER_LIST *list);
-
-void load_users_dat(char *filename, USER_LIST *list);
-
-void write_users_dat(char *filename, USER_LIST *list);
+struct USER_NODE *get_tail(USER_LIST *list);
 
 void login(void);
 
@@ -121,6 +126,8 @@ void main_menu(void);
 void not_implemented(void);
 
 void print_users(USER_LIST *list);
+
+int search(USER *list, char *username, char *prontuario, int pos_inicial, int pos_final);
 
 void sort_list(USER_LIST *list);
 
@@ -179,6 +186,45 @@ USER_LIST *build_user_list(int list_size)
 }
 
 
+/* Carrega dados em memória e inicializa o programa */
+void initialize(void)
+{
+    int list_size;  /* Tamanho da lista de usuários. Este valor depende do tamanho do arquivo USUÁRIOS.DAT. */
+
+    /* Verifica se o arquivo USUARIOS.DAT já existe. Caso contrário, cria um novo arquivo com os registros padrão. */
+    if (!file_exists("USUARIOS.DAT"))
+    {
+        printf("Criando arquivo USUARIOS.DAT com registros padrão.\n");
+        build_user_dat();
+
+        /* Verifica se o arquivo foi gerado corretamente */
+        if (file_exists("USUARIOS.DAT"))
+        {
+            printf("Arquivo USUARIOS.DAT criado com sucesso.\n");
+            printf("Tamanho do USUARIOS.DAT: %d bytes\n", file_size("USUARIOS.DAT"));
+        }
+
+        else
+        {
+            system("cls");
+            printf("*** Erro ao criar arquivo USUARIOS.DAT ***");
+            getch();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* Aloca lista e carrega dados do arquivo USUARIOS.DAT na memória */
+    list_size = (file_size("USUARIOS.DAT")/sizeof(USER));
+
+    users = build_user_list(list_size);
+    validate_allocation(users);
+
+    load_users_dat("USUARIOS.DAT", users);
+    validate_allocation(users);
+
+    print_users(users);
+}
+
 /* Retorna o tamanho de uma lista */
 /* TODO: passar um void pointer e o tipo da lista. Dessa forma não é preciso fazer outra função para a lista de produtos.
     ex: int get_size(void *list, char list_type) */
@@ -188,7 +234,7 @@ int get_size(USER_LIST *list)
     int size = 0;
     struct USER_NODE *current_node = list->head;
 
-    while (1)
+    while (TRUE)
     {
 
         if (current_node == NULL) /* Lista possui zero elementos */
@@ -254,18 +300,17 @@ void build_user_dat(void)
     };
 
 
-
-    /* Informa o usuário caso o arquivo já exista */
+    /* Verifica se o arquivo USUARIOS.DAT já existe. Caso positivo, exibe um alerta. */
     if (file_exists("USUARIOS.DAT"))
     {
         printf("Arquivo USUARIOS.DAT já existe!\nDeseja sobrescrever?\n");
-        printf("S = Sim\nN = Não\n\n");
+        printf("S = Sim\nN = Não\nEscolha uma opção:\t");
 
         if (getche() != 'S')
         {
             printf("\nOperação de escrita cancelada\n");
             getch();
-            exit_program();
+            return;
         }
 
         else
@@ -338,21 +383,22 @@ void write_users_dat(char *filename, USER_LIST *list)
     FILE *userDAT;
     struct USER_NODE *current_user = list->head;
     USER *user_data;
-    char choice;
+    // char choice;
 
     /* Informa o usuário caso o arquivo já exista */
     if (file_exists(filename))
     {
         printf("Arquivo %s já existe!\nDeseja sobrescrever?\n", filename);
         printf("S = Sim\nN = Não\n\n");
-        choice = getche();
+        // choice = getche();
 
-        if (choice != 'S')
+        if (getche() != 'S')
         {
             printf("\nOperação de escrita cancelada\n");
             getch();
-            exit_program();
+            return;
         }
+
         else
             printf("\nSobrescrevendo arquivo %s\n\n", filename);
     }
@@ -386,14 +432,15 @@ void print_users(USER_LIST *list)
 {
     struct USER_NODE *current_user = list->head;
 
-    printf("Valores na lista:\n\n");
-    while(1)
+    printf("Exibindo lista de usuários:\n\n");
+
+    while(TRUE)
     {
         printf("%d. %s - %s\n", current_user->position, current_user->data.username, current_user->data.prontuario);
 
         if (current_user->next == NULL)
         {
-            printf("Fim da lista!\n\n");
+            printf("\nFim da lista!\n\n");
             break;
         }
 
@@ -442,7 +489,10 @@ void exit_program(void)
 }
 
 
-/* Exibe menu principal */
+/* Exibe menu principal
+
+    OBS: As opções de abastecer gôndolas e gerenciar usuários deve aparecer apenas para o usuário master.
+*/
 void main_menu(void)
 {
     char choice;
@@ -451,11 +501,9 @@ void main_menu(void)
     {
         system("cls");
         printf("SISTEMA GERENCIADOR DE SUPERMERCADO - VERSÃO BETA\n\n");
-        /*if (DEBUG_MODE == TRUE)
-        {
+        if (DEBUG_MODE)
             printf("======== OPERANDO EM MODO DEBUG ==============\n\n");
-            printf("X. EXECUTAR ROTINA DE TESTES\n\n");
-        } */
+
         printf("1. Abastecer gôndolas\n");
         printf("2. Caixa/PDV\n");
         printf("3. Gerenciar usuários\n\n");
@@ -463,7 +511,8 @@ void main_menu(void)
         printf("Selecione uma opção: ");
 
         fflush(stdin);
-        choice = getche();
+        choice = getche(); // OBS: testar o getche() diretamente ao invés de declarar variável. ex: switch(getche())
+
 
         switch(choice)
         {
@@ -492,33 +541,54 @@ void main_menu(void)
                 continue;
         }
 
-    } while(choice != '0');
+    } while(choice != '0'); /* OBS: Seria possível usar um (while TRUE) aqui para garantir que o programa só encerre após o input do usuário? */
 }
 
 
 /* Gerencia login de usuário.
-Sugestão: ler parametros ao invés de usar scanf. */
+Sugestão: ler parametros ao invés de usar gets(). */
 void login(void)
 {
+    USER *temp_user_list;
+    struct USER_NODE *current_node = users->head;
     char username[USERNAME_SIZE];
     char prontuario[PRONTUARIO_SIZE];
+    int i;
+    int list_size = get_size(users);
+
+    /*  Para facilitar a busca binária recursiva, é melhor alocar um vetor estático. Dessa forma é possível fazer a busca usando índices ao invés de lidar com os ponteiros da lista dinâmica */
+
+    temp_user_list = (USER *) malloc(list_size * sizeof(USER));
+
+    for (i = 0; i < list_size; i++)
+    {
+        temp_user_list[i] = current_node->data;
+        current_node = current_node->next;
+
+        if (DEBUG_MODE)
+            printf("Usuário copiado: %s - %s\n", temp_user_list[i].username, temp_user_list[i].prontuario);
+    }
 
     system("cls");
+
     printf("Informe seu nome de usuário:\n");
-    printf("(O nome de usuário padrão é seu primeiro nome em letras maiúsculas)\n");
     gets(username);
+
     printf("\n\nInforme seu prontuário:\n");
     gets(prontuario);
 
-    if (DEBUG_MODE == TRUE)
-    {
-        printf("\nNome informado: %s\n", username);
-        printf("Prontuário informado: %s\n", prontuario);
-    }
+    if (search(temp_user_list, username, prontuario, 0, (list_size - 1)))
+        printf("Registro encontrado: %s - %s", username, prontuario);
+
+    else
+        printf("Registro NÃO ENCONTRADO: %s - %s", username, prontuario);
+
+    /* Após as operações, libera a memória ocupada pela lista temporária. */
+    free(temp_user_list);
 }
 
 
-/* Verifica se o arquivo foi escrito corretamente. */
+/* Verifica se um arquivo foi escrito ou lido corretamente. Caso negativo, encerra o programa. */
 void validate_file(FILE *file)
 {
     if (file == NULL || ferror(file))
@@ -544,50 +614,37 @@ void validate_allocation(void *object)
 }
 
 
+/* Busca um registro de usuário usando busca binária recursiva. Caso encontrado, returna TRUE; do contrário retorna FALSE. */
+int search(USER *list, char *username, char *prontuario, int pos_inicial, int pos_final)
+{
+    int meio = (pos_inicial + pos_final) / 2;
+    int strcmp_result;
 
-/*  Busca um prontuário na lista de registros
-    OBS: FUNÇÃO OBSOLETA. É necessário adaptar para uso com lista encadeada */
+    /* Primeiro testamos o prontuário. Caso exista, testamos o username. */
+    if (list[meio].prontuario == prontuario)
+    {
+        if (list[meio].username == username)
+            return(TRUE);
+    }
 
-// void busca_prontuario(char *prontuario, char tipo_busca)
-// {
-    /*  TIPOS DE BUSCA:
+    else
+    {
+        /* Comparamos o valor do meio com o nome fornecido. Se strcmp retornar um valor positivo, a segunda string vem antes da primeira. */
+        strcmp_result = strcmp(list[meio].username, username);
 
-        L: Busca linear
-        B: Busca binária (não implementado)
-    */
+        /* Caso tenhamos chegado ao fim do vetor e não encontramos o valor, retorna FALSE */
+        if (pos_inicial > pos_final)
+            return(FALSE);
 
-    // USER *user_pointer = user_list; /* Aponta para o primeiro registro de usuário */
-    // int found = FALSE; /* Flag para indicar se o prontuario foi encontrado */
+        /* Se o usuário procurado está ANTES do meio, procuramos à esquerda (de 0 até meio - 1) */
+        else if (strcmp_result > 0)
+            search(list, username, prontuario, 0, (meio - 1));
 
-    // switch(tipo_busca)
-    // {
-        // case 'L': /* Busca linear */
-        // {
-
-            // for(; user_pointer < &user_list[user_list_size()]; user_pointer++)
-            // {
-                // if (strcmp(prontuario, user_pointer->prontuario) == 0)
-                // {
-                    // printf("Prontuário encontrado: %s pertence à %s\n",
-                        // user_pointer->prontuario, user_pointer->username);
-                    // found = TRUE;
-                    // break;
-                // }
-            // }
-
-            // if (found == FALSE)
-                // printf("Prontuário não encontrado: %s\n", prontuario);
-            // break;
-        // }
-
-        // default:
-        // {
-            // printf("ERRO - Parâmetro inválido na função de busca [%c]", tipo_busca);
-            // getch();
-            // exit(EXIT_FAILURE);
-        // }
-    // }
-// }
+        /* Se o usuário procurado está DEPOIS do meio, procuramos à direita (de meio + 1 até posição final) */
+        else if (strcmp_result < 0)
+            search(list, username, prontuario, (meio + 1), pos_final);
+    }
+}
 
 
 /* Ordena lista em ordem crescente de nomes usando bubble sort */
@@ -611,7 +668,7 @@ void sort_list(USER_LIST *list)
             next_user = current_user->next;
             strcmp_result = strcmp(current_user->data.username, next_user->data.username);
 
-            /* strcmp retorna um valor positivo quando a segunda string vem antes da primeira.  */
+            /* strcmp retorna um valor positivo quando a segunda string vem antes da primeira. */
             if (strcmp_result > 0)
             {
 
@@ -636,6 +693,58 @@ void sort_list(USER_LIST *list)
 }
 
 
+/* Adiciona um novo usuário a uma lista pré-alocada */
+void add_user(USER_LIST *list)
+{
+    struct USER_NODE *last_node = get_tail(list);
+    struct USER_NODE *new_node;
+    char username[USERNAME_SIZE];
+    char prontuario[PRONTUARIO_SIZE];
+
+    if (DEBUG_MODE)
+        printf("Último registro: %s - %s\n", last_node->data.username, last_node->data.prontuario);
+
+    printf("Adicionando novo registro de usuário:\n\n");
+    printf("Insira o nome de usuário:\t");
+    gets(username);
+
+    printf("\nInsira o prontuário\t");
+    gets(prontuario);
+
+    /* Aloca um novo nó na última posição */
+    last_node->next = (struct USER_NODE *) malloc(sizeof(USER_NODE));
+    validate_allocation(last_node->next);
+
+    /* Aqui seria possível usar last_node = last_node->next ao invés de declarar uma nova variável. */
+    new_node = last_node->next;
+
+    /* O último nó deve apontar para NULL. */
+    new_node->next = NULL;
+    new_node->position = last_node->position + 1;
+
+    /* Copia os valores para o campo relevante */
+    strcpy(new_node->data.username, username);
+    strcpy(new_node->data.prontuario, prontuario);
+
+    printf("Usuário adicionado: %s - %s\n", new_node->data.username, new_node->data.prontuario);
+
+    /* Após adicionar usuário, reorganiza a lista */
+    sort_list(list);
+}
+
+
+/* Retorna o último elemento de uma lista */
+struct USER_NODE *get_tail(USER_LIST *list)
+{
+    struct USER_NODE *last_node = list->head;
+
+    while (last_node->next != NULL)
+        last_node = last_node->next;
+
+    return(last_node);
+}
+
+
 /* Retorna TRUE caso o arquivo exista no disco, do contrário retorna FALSE; */
 int file_exists(char *filename)
 {
@@ -650,6 +759,7 @@ int file_exists(char *filename)
         return(TRUE);
     }
 }
+
 
 /* Retorna o tamanho de um arquivo em bytes */
 long int file_size(char *filename)
@@ -671,7 +781,7 @@ long int file_size(char *filename)
         printf("ERRO - Arquivo não encontrado: %s\n", filename);
 }
 
-/* Troca dois usuários de posição */
+/*  Troca dois registros de usuário de posição */
 void swap_users(USER *user1, USER *user2)
 {
     USER temp_user;
